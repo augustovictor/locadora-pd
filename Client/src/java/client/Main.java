1b33765997a8b43c334ccdd29c9900b0d77d483b
Victor <victoraweb@gmail.com>
2015-06-30 16:47:45 -0300

    Main code refactoring

--- Client/src/java/client/Main.java
+++ Client/src/java/client/Main.java
@@ -17,42 +17,9 @@ import sessionBean.LibraryRemote;
 public class Main {
     @EJB
     private static LibraryRemote libRemote;
-    
-    
 
     public static void main(String[] args) {
         // Alterar persistence, pois esta drop and create
-        
-//        libRemote.addBook(1, "Testando titulo", "Abril", 4161L, 2, "Victor");
-//        libRemote.addBook(2, "Como ficar monstro em duas refeições", "Monster gym", 4162L, 1, "Gleidson");
-//        libRemote.addBook(3, "Lataria nova", "Editora Carro", 4163L, 1, "Victor");
-//        libRemote.addBook(4, "Lataria nova2", "Editora Carro", 4164L, 2, "Mama");
-//        System.out.println("Livros by Victor");
-//        System.out.println(libRemote.findBookByAuthor("Victor"));
-
-//        libRemote.updateBook(2, "Título novo2", "Abril", 4162L, 2, "Gleidson");
-//        System.out.println("Todos os livros:");
-
-//        System.out.println(libRemote.findAllBooks()); // List all books
-//        
-//        libRemote.deleteBook(4162L); // Delete by isbn
-//        
-//        System.out.println("Todos os livros apos remover 4162L:");
-//        System.out.println(libRemote.findAllBooks()); // List all books
-//        
-//        System.out.println("Books by isbn");
-//        System.out.println(libRemote.findBookByIsbn(4161));
-//        
-//        System.out.println("Books by title");
-//        System.out.println(libRemote.findBookByTitle("Editora Carro"));
-//        
-//        libRemote.deleteBookByTitle("Editora Carro"); // Delete by title
-//        
-//        System.out.println("Todos os livros apos remover \"Editora Carro\":");
-//        System.out.println(libRemote.findAllBooks()); // List all books
-//        
-//        System.out.println(libRemote.qtdSearch());
-        
         
         Scanner keyboard = new Scanner(System.in);
         
@@ -199,9 +166,7 @@ public class Main {
                 
                     
                 // OPTION 9 - 
-            }
-        }
-          
-        
-    }
-}
+            } // End switch
+        } // End while
+    } // End main()
+} // End Main
