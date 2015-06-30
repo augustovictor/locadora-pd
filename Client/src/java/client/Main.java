3125632b6bd5e71e3738e5b88aa4a95004b64fcc
Victor <victoraweb@gmail.com>
2015-06-29 21:52:03 -0300

    Find and remove by title

--- Client/src/java/client/Main.java
+++ Client/src/java/client/Main.java
@@ -20,17 +20,31 @@ public class Main {
     public static void main(String[] args) {
         // Alterar persistence, pois esta drop and create
         
-        libRemote.addBook(1L, "Testando titulo", "Abril", 4161L, 2, "Victor");
-        libRemote.addBook(2L, "Como ficar monstro em duas refeições", "Monster gym", 4162L, 1, "Gleidson");
-        libRemote.addBook(3L, "Lataria nova", "Editora Carro", 4163L, 1, "Victor");
+        libRemote.addBook(1, "Testando titulo", "Abril", 4161L, 2, "Victor");
+        libRemote.addBook(2, "Como ficar monstro em duas refeições", "Monster gym", 4162L, 1, "Gleidson");
+        libRemote.addBook(3, "Lataria nova", "Editora Carro", 4163L, 1, "Victor");
+        libRemote.addBook(4, "Ficando grande", "Monster gym", 4164L, 1, "Mama");
+        System.out.println("Livros by Victor");
         System.out.println(libRemote.findBookByAuthor("Victor"));
-        libRemote.updateBook(2L, "Título novo2", "Abril", 4162L, 2, "Gleidson");
+        libRemote.updateBook(2, "Título novo2", "Abril", 4162L, 2, "Gleidson");
         System.out.println("Todos os livros:");
         System.out.println(libRemote.findAllBooks()); // List all books
+        
         libRemote.deleteBook(4162L); // Delete by isbn
-//        libRemote.deleteBookByTitle("Como ficar monstro em duas refeições"); // Delete by title
+        
+        System.out.println("Todos os livros apos remover 4162L:");
+        System.out.println(libRemote.findAllBooks()); // List all books
+        
         System.out.println("Books by isbn");
         System.out.println(libRemote.findBookByIsbn(4161));
+        
+        System.out.println("Books by title");
+        System.out.println(libRemote.findBookByTitle("Ficando grande"));
+        
+        libRemote.deleteBookByTitle("Ficando grande"); // Delete by title
+        
+         System.out.println("Todos os livros apos remover \"Ficando grande\":");
+        System.out.println(libRemote.findAllBooks()); // List all books
     }
     
 }
